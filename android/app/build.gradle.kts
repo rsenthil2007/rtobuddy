6ed329456529be428c8d1573e31b6903aa92b0ed
rsenthil2007 <rsenthil2007@gmail.com>
2026-08-20 13:59:50 +0530

    Add CI pipeline to generate basic Android debug APK.
    This enables APK creation from git without requiring local Java/Android SDK setup.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.serialization")
@@ -45,10 +46,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     packaging {
